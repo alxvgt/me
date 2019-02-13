f0190a6e52cbcfa68ea5697f07dad831f8a456c8
alxvgt <1221867+alxvgt@users.noreply.github.com>
2019-02-13 13:38:55 +0100

    Update service-worker.js

--- site/js/service-worker.js
+++ site/js/service-worker.js
@@ -1,7 +1,9 @@
 const staticAssets = [
     '../',
-    '../css/*',
-    '../js/*'
+    '../css/bootstrap-4.2.1-dist/css/bootstrap.min.css',
+    '../fontawesome/css/all.css',
+    '../css/me.css',
+    '../js/app.js'
 ];
 
 self.addEventListener('install', async event => {
